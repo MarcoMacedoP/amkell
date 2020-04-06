91a1645d38dd30f848505025a9f8e9606804526d
Marco <marcosiegman01@gmail.com>
2020-04-06 18:37:14 -0500

    doned HomeSlider

---
+++ src/components/HomeSlider.tsx
@@ -0,0 +1,81 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+type Props = {};
+type HomeSliderType = React.FC<Props>;
+
+export const HomeSlider: HomeSliderType = (props) => {
+  return (
+    <nav className="h-middle w-screen -ml-5">
+      <div className="h-40 bg-black h-2/3 p-2">
+        <div className="flex flex-col w-full justify-center items-center h-full">
+          <div className="line mb-2" />
+          <h1 className="text-xl text-white uppercase text-center">
+            Calidad e innovacion arquitectonica en la edificación
+          </h1>
+          <button className="hidden my-4 text-primary uppercase bg-white opacity-75 border-primary border-2 py-2 rounded-lg w-2/3">
+            Nuestros proyectos
+          </button>
+          <div className="w-full flex text-primary items-center justify-center mt-2">
+            <span className="material-icons text-4xl">keyboard_backspace</span>
+            <span className="material-icons text-4xl rotate-180 transform">
+              keyboard_backspace
+            </span>
+          </div>
+        </div>
+      </div>
+      <div className="h-1/3 w-full flex flex-wrap">
+        <SectionLink
+          to={"/proyectos"}
+          isActive={true}
+          index={1}
+          title={"Innovacion arquitectonica"}
+        />
+        <SectionLink
+          to={"/materiales"}
+          isActive={false}
+          index={2}
+          title={"Materiales innovadores"}
+        />
+        <SectionLink
+          to={"/sistemas"}
+          isActive={false}
+          index={3}
+          title={"Sistemas innovadores"}
+        />
+        <SectionLink
+          to={"/calidad"}
+          isActive={false}
+          index={4}
+          title={"Calidad arquitectonica"}
+        />
+        <SectionLink
+          to={"/proyectos"}
+          isActive={false}
+          index={5}
+          title={"Interior innovador"}
+        />
+      </div>
+    </nav>
+  );
+};
+
+type SectionLinkProps = {
+  to: string;
+  isActive: boolean;
+  index: number;
+  title: string;
+};
+const SectionLink = ({ to, isActive, index, title }: SectionLinkProps) => (
+  <Link
+    to={to}
+    className={`w-2/6 h-full flex flex-col text-sm justify-center px-2 transition-colors duration-300 ease-in ${
+      isActive
+        ? "bg-primary text-white hover:bg-red-700"
+        : "bg-white text-black hover:bg-gray-300"
+    }`}
+  >
+    <span>0{index}</span>
+    <span>{title}</span>
+  </Link>
+);
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,13 @@
 import React from "react";
-
+import { HomeSlider } from "../components/HomeSlider";
 type Props = {};
 type HomeType = React.FC<Props>;
 
 export const HomePage: HomeType = (props) => {
   console.log(props);
   return (
-    <div>
-      <p>Pagina de inicio</p>
-    </div>
+    <>
+      <HomeSlider />
+    </>
   );
 };
